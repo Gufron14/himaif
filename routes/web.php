5606702279d16641ef60d27c18869b080f77c1c4
Gufron14 <gupron.nurjalil14@gmail>
2023-07-05 01:16:29 +0700

    route announ dan category

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AnnouncementController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,4 +21,18 @@ Route::get('/', function () {
 });
 
 Route::get('/index', [DashboardController::class, 'dashboard']);
-Route::get('/announcement', [DashboardController::class, 'announcement']);
+Route::get('/announcement', [AnnouncementController::class, 'index']);
+
+Route::get('/announcement/add', [AnnouncementController::class, 'create']);
+Route::post('/announcement', [AnnouncementController::class, 'store']);
+
+Route::get('/announcement/{id}/edit', [AnnouncementController::class, 'edit']);
+Route::put('/announcement/{id}', [AnnouncementController::class, 'update'])->name('announcement.update');
+
+Route::delete('/announcement/{id}', [AnnouncementController::class, 'destroy'])->name('announcement.destroy');
+
+Route::get('/category', [CategoryController::class, 'index']);
+
+Route::get('/category/add', [CategoryController::class, 'create']);
+Route::post('/category', [CategoryController::class, 'store']);
+
